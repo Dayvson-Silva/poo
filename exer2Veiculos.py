7af427fad682a24b19ed0c48f0ec4d06be76cba5
Dayvson silva <dayvsonsilva681@gmail.com>
2024-11-04 13:36:56 -0300

    atualização

--- exer2Veiculos.py
+++ exer2Veiculos.py
@@ -1,5 +1,7 @@
 opt = int(input("1 - carro \n2 - bike \nEscolha: "))
 
+
+
 class Veiculo:
     def __init__(self):
         self.name = str(input(f"Me informe o nome do seu Veículo :"))
@@ -7,31 +9,32 @@ class Veiculo:
         self.model = str(input(f"Me informe o modelo do seu Veículo :"))
     
      
+match opt:
+    case 1:
           
-class Car(Veiculo):
-    def __init__(self):
-        super().__init__()
-    def info_car(self):
-        return f"O nome do seu Veículo é {self.name} com a cor {self.color} e o modelo {self.model}"
+        class Car(Veiculo):
+            def __init__(self):
+                super().__init__()
+            def info_car(self):
+                return f"O nome do seu Veículo é {self.name} com a cor {self.color} e o modelo {self.model}"
   
+my_car = Car() 
 
-class Bike(Veiculo):
-    def __init__(self):
-        super().__init__()
-    def info_bike(self):
-        return f"O nome do seu Veículo é {self.name} com a cor {self.color} e o modelo {self.model}"
+
+match opt :
+    case 2:
+
+        class Bike(Veiculo):
+            def __init__(self):
+                super().__init__()
+            def info_bike(self):
+                return f"O nome do seu Veículo é {self.name} com a cor {self.color} e o modelo {self.model}"
 
        
     
      
 my_bike = Bike()
-my_car = Car() 
-match opt:
-    case 1:
-        if opt == 1:
-            print(my_car.info_car())
-        else:
-            print(my_bike.info_bike())
+
             
         
 
